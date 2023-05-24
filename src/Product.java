bfe435f15f41d4c431efd2c87c0a539194522989
MohanadMakhzoumi <m.makhzome@gmail.com>
2023-05-24 09:58:28 +0300

    Addquantity

--- src/Product.java
+++ src/Product.java
@@ -1,10 +1,12 @@
 public class Product {
     private String name;
     private double price;
+    private int quantity;
 
-    public Product(String name, double price) {
+    public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
+        this.quantity = quantity;
     }
 
     public String getName() {
@@ -13,5 +15,13 @@ public class Product {
 
     public double getPrice() {
         return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
